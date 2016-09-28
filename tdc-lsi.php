f6d509e2435e8616220253da4f2faeca3cad42c5
TheEdu <edigde@gmail.com>
2016-09-28 05:18:48 -0300

    Agregada funcion estado(ip)

--- tdc-lsi.php
+++ tdc-lsi.php
@@ -17,12 +17,43 @@
 
     	.encabezado-tabla {
   			background-color: #6699ff;
-  			background-color: #FFF;
+
   			border-style: solid; 
   			border-width: 2px;
 		}
 
     </style>
+
+
+  <?php
+
+    function estado($ip)
+      {
+        $aux = "ping ";
+        $comando = $aux . $ip;
+
+        set_time_limit(0);
+        $ping = shell_exec($comando);
+
+        $recibidos4 = strpos($ping, "recibidos = 4");
+        $recibidos0 = strpos($ping, "recibidos = 0");
+        $inaccesible = strpos($ping, "Host de destino inaccesible");
+
+        if ($recibidos4 && !($inaccesible)) {
+          //Entra aca si se reciben los 4 paquetes, 
+          //y ninguno de los paquetes es respuesta del gateway con la leyenda "Host de destino inaccesible"
+            $res = "Up!"; // Color Verde
+        }elseif(!($recibidos4) && !($recibidos0)) {
+          //Si hubo perdida parcial de paquetes, entra aca
+          $res = "Unknown!"; // Color Amarillo
+        }else{
+          //Si hubo perdida total de paquetes / el host es inaccesible, entra aca
+            $res = "Down!"; // Color Rojo
+        };
+
+         return $res;
+      };
+  ?>
 
 </head>
 <body>
@@ -42,75 +73,89 @@
   					</tr>
   				</thead>
   				<tbody>
-  					<tr class="success">
+  					<tr>
   						<th class="text-center col-xs-3">ROUTER-LSI</th>
   						<td class="text-center col-xs-3">192.168.1.20</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.20"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					<tr class="success">
+  					<tr>
   						<th class="text-center col-xs-3">ROUTER-REDES</th>
   						<td class="text-center col-xs-3">192.168.1.21</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.21"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					<tr class="success">
+  					<tr>
   						<th class="text-center col-xs-3">ROUTER-4TO</th>
   						<td class="text-center col-xs-3">192.168.1.22</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.22"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					  <tr class="danger">
+  					  <tr>
   						<th class="text-center col-xs-3">ROUTER-1RO-2DO</th>
   						<td class="text-center col-xs-3">192.168.1.23</td>
-  						<td class="text-center col-xs-3"><strong>Down!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.23"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					 <tr class="success">
+  					 <tr>
   						<th class="text-center col-xs-3">ROUTER-PEDRO</th>
   						<td class="text-center col-xs-3">192.168.1.24</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.24"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					  <tr class="success">
+  					  <tr>
   						<th class="text-center col-xs-3">ROUTER-LSI2</th>
   						<td class="text-center col-xs-3">192.168.1.25</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.25"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-  					  <tr class="success">
+  					  <tr>
   						<th class="text-center col-xs-3">SrvLsi-01</th>
   						<td class="text-center col-xs-3">192.168.1.35</td>
-  						<td class="text-center col-xs-3"><strong>Down!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.35"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-   					<tr class="success">
+   					<tr>
   						<th class="text-center col-xs-3">SrvLsi-02</th>
   						<td class="text-center col-xs-3">192.168.1.45</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.45"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-   					<tr class="success">
+   					<tr>
   						<th class="text-center col-xs-3">PrefecturaVM</th>
   						<td class="text-center col-xs-3">192.168.1.55</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.55"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
   					</tr>
 
-   					<tr class="success">
+   					<tr>
   						<th class="text-center col-xs-3">SrvRedes</th>
   						<td class="text-center col-xs-3">192.168.1.72</td>
-  						<td class="text-center col-xs-3"><strong>Up!</strong></td>
+  						<td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.1.72"); ?>  </strong></td>
   						<td class="text-center col-xs-3">Today</td>
-  					</tr>  
+  					</tr> 
+
+            <tr>
+              <th class="text-center col-xs-3">miCompu</th>
+              <td class="text-center col-xs-3">192.168.10.100</td>
+              <td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.10.100"); ?>  </strong></td>
+              <td class="text-center col-xs-3">Today</td>
+            </tr> 
+
+            <tr>
+              <th class="text-center col-xs-3">otraCompu</th>
+              <td class="text-center col-xs-3">192.168.10.111</td>
+              <td class="text-center col-xs-3"><strong>  <?php echo estado("192.168.10.111"); ?>  </strong></td>
+              <td class="text-center col-xs-3">Today</td>
+            </tr>  
 
   				</tbody>
 			</table>
