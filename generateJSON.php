33741975f550c1427d66de107caea6c8a2fcced3
Matías Mangiantini <matiasm15@hotmail.com>
2016-10-22 03:09:44 -0300

    agregado actualizacion automatica con ajax

--- generateJSON.php
+++ generateJSON.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 	 function fecha(){
-	 	  
+
     	  $time = time();
       	  return date("d-m-Y (H:i:s)", $time);
      };
@@ -29,21 +29,16 @@
 	 		$comando = "ping -c 1 " . $ip . " > /dev/null ; echo $?";
 	 		set_time_limit(30); //resetea el tiempo de ejecutcion de el script cada vez que se ejecuta esta funcion
         	$errorCode_ping = shell_exec($comando);
-        	
-        	if ($errorCode_ping != null && $errorCode_ping == 0) {
-        		$host_state = '<i class="fa fa-circle fa-3x  aria-hidden="true" style="color:00FF00;" ></i>';
-        	} else {
-        		$host_state = '<i class="fa fa-circle fa-3x" aria-hidden="true" style="color:FF0000;" ></i>';
-        	}
 
+    	$host_state = ($errorCode_ping != null && $errorCode_ping == 0 ? 'up' : 'down';
 
 	 		$host_name = $row_array[1];
 	 		$host_name = str_replace("\r" , "" , $host_name);
 	 		$host_name = str_replace("\n" , "" , $host_name);
-	 		
-	 		
-	 		$miArray = array("ip"=>$ip,"host_name"=> $host_name, "host_state"=> $host_state,"host_date"=> fecha());
-	 		
+
+
+	 		$miArray = array("ip"=>$ip,"name"=> $host_name, "state"=> $host_state,"date"=> fecha());
+
 	 		$preJSON[] = $miArray;
 	 	}
 	 }
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <title>TDC-LSI</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
-
+    <script src="js/jquery-2.2.4.min.js"></script>
 
     <style>
     	.margen-medio{
@@ -23,14 +23,22 @@
     	.encabezado-tabla{
   			background-color: #6699ff;
 
-  			border-style: solid; 
+  			border-style: solid;
   			border-width: 2px;
 		}
 
       .table > tbody > tr > td.vert-align,.table > thead > tr > th.vert-align{
         vertical-align: middle;
         text-align: center;
-    }
+      }
+
+      .host.up .state .fa-circle {
+        color: #00ff00;
+      }
+
+      .host.down .state .fa-circle {
+        color: #ff0000;
+      }
 
     </style>
 
@@ -38,13 +46,13 @@
 </head>
 <body>
 	<div class="container-fluid " style="border-style: solid; border-width: 5px; ">
-		
+
 
 		<h1 class="text-center margen-medio">Tablero de Control - LSI</h1>
 
 		<div class="table-responsive margen-grande">
 			<table class="table table-striped table-hover table-bordered">
-  				<thead> 
+  				<thead>
   					<tr class="encabezado-tabla">
   						<th class="vert-align col-xs-3">Dispositivo</th>
   						<th class="vert-align col-xs-3">Estado</th>
@@ -52,25 +60,6 @@
   					</tr>
   				</thead>
   				<tbody>
-
-
-          <?php
-          
-            $path = "hostsList.json";
-            $data = file_get_contents($path);
-            $json = json_decode($data, true);
-            
-            foreach ($json['hosts'] as $row) {
-              echo "<tr>";
-              echo   "<td class=\"vert-align col-xs-3\" >" . $row['host_name']    .   "</td>";
-              echo   "<td class=\"vert-align col-xs-3\" >" . $row['host_state']   .   "</td>";
-              echo   "<td class=\"vert-align col-xs-3\">" .  $row['host_date']   .   "</td>";
-              echo "</tr>";
-             } 
-
-          ?> 
-
- 
 
   				</tbody>
 			</table>
@@ -81,4 +70,36 @@
 
 
 </body>
+
+<script type="text/javascript">
+  function actualizar() {
+    $.post('hostsList.json', function(response) {
+      $('tbody').empty();
+
+      var hosts = response.hosts;
+
+      for (var i = 0; i < hosts.length; i++) {
+        var tr = $('<tr>').addClass('host ' + hosts[i].state);
+
+        var tdName = $('<td>').addClass('vert-align col-xs-3 name').text(hosts[i].name);
+        var tdState = $('<td>').addClass('vert-align col-xs-3 state').html('<i class="fa fa-circle fa-3x"></i>');
+        var tdDate = $('<td>').addClass('vert-align col-xs-3 date').text(hosts[i].date);
+
+        tr.append(tdName);
+        tr.append(tdState);
+        tr.append(tdDate);
+
+        $('tbody').append(tr);
+      }
+    })
+  }
+
+  $(function() {
+    actualizar();
+
+    /* Actualiza la información cada 5 segundos. */
+    setInterval(actualizar, 5000);
+  });
+</script>
+
 </html>
